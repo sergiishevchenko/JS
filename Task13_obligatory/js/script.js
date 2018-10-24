50328d05aa4ff4a09dc68be51859e7f473968b5b
sergii <sergiishevchenko017@gmail.com>
2018-10-24 23:33:14 +0300

    second commit

--- Task13_obligatory/js/script.js
+++ Task13_obligatory/js/script.js
@@ -353,7 +353,6 @@ function checkTel(){
 }
 
 function check(){
-    if (event.keyCode == 101 || event.keyCode == 43 || event.keyCode == 44 || event.keyCode == 46
-         && event.keyCode < 48 || event.keyCode > 57)
+    if (event.keyCode < 48 || event.keyCode > 57)
     event.preventDefault();
 }
